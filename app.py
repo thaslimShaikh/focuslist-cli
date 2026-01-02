b9bf894f566d72eb7ddec129f0c60f8a20f8a059
thaslimShaikh <786shaikthaslim@gmail.com>
2026-01-02 21:58:42 +0530

    Add option to mark task as completed

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ def show_menu():
     print("3. Exit")
 
 show_menu()
+
+def complete_task(index):
+    with open("tasks.txt", "r") as f:
+        tasks = f.readlines()
+
+    tasks[index] = tasks[index].replace("pending", "done")
+
+    with open("tasks.txt", "w") as f:
+        f.writelines(tasks)
+
+    print("Task marked as done")
+
